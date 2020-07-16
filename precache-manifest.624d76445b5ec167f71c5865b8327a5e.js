e500875947e0ae452856c9ee5834172ba4205070
boydungbg <dunglc.nde18055@vtc.edu.vn>
2020-07-16 15:17:33 +0700

    ok

---
+++ precache-manifest.624d76445b5ec167f71c5865b8327a5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1106e6cd9266417e0fcb7e3f4ad9f30",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c97673f36ca3ed7192c1",
+    "url": "./static/css/main.9c9da49a.chunk.css"
+  },
+  {
+    "revision": "0a43043e415a47a45955",
+    "url": "./static/js/2.6142576a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.6142576a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c97673f36ca3ed7192c1",
+    "url": "./static/js/main.c3e496b3.chunk.js"
+  },
+  {
+    "revision": "3e7eaac669c702e669b0",
+    "url": "./static/js/runtime-main.30ba4255.js"
+  }
+]);
